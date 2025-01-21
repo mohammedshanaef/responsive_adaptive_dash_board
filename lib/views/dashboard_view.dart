9dc54d20f02f7ba676d975a7d77c45b6624fac99
mohammedshanaef <mohammed.shaneaf.963@gmail.com>
2025-01-21 10:26:46 +0300

    create dashboard desektop layout

--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_adaptive_dash_board/views/widgets/adaptive_layout.dart';
+import 'package:responsive_adaptive_dash_board/views/widgets/dashboard_desektop_layout.dart';
 
 class DashBoardView extends StatelessWidget {
   const DashBoardView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: AdaptiveLayout(
+        mobileLayout: (context) => SizedBox(),
+        tabletLayout: (context) => SizedBox(),
+        desektopLayout: (context) => DashboardDesektopLayout(),
+      ),
+    );
   }
 }
---
+++ lib/views/widgets/dashboard_desektop_layout.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DashboardDesektopLayout extends StatelessWidget {
+  const DashboardDesektopLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
